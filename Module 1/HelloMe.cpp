9f4499fd0439b9c98147b296429fa63595135c64
Ruben Mejia <rubenmejiac@gmail.com>
2021-07-15 15:51:50 -0400

    Add files via upload

---
+++ Module 1/HelloMe.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/* Ruben Mejia Corleto
+   CS 116 - Professor Flores
+   Module 1 Assignment - HelloMe Quiz 
+   This program displays my first and last names
+   02/10/2021
+*/
+
+int main() {
+  cout << "Hello, my name is Ruben Mejia Corleto\n";
+}
+
+/* Output:
+ clang++-7 -pthread -std=c++17 -o main main.cpp
+ ./main
+ Hello, my name is Ruben Mejia Corleto
+*/
